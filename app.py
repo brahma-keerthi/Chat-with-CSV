41d48aa6aeb10d62c06b4f1cb4d57472186c4848
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2023-12-04 00:27:41 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
-from dotenv import load_dotenv
 
 def main():
-    load_dotenv()
+    OPENAI_API_KEY=st.secrets['OPENAI_API_KEY']
 
     st.set_page_config(page_title="Ask your CSV")
     st.header("Ask your CSV 📊")
